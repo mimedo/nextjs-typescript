846eadfbb60807d1383cddeb0fb7dddadf4659e1
Miroslav Cutic <miroslav.cutic@cleoag.ch>
2021-10-27 16:22:19 +0200

    Changed names and added new path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
-import Footer from '@components/footer/footer'
-import Header from '@components/header/header'
+import Footer from '@components/common/Footer/Footer'
+import Header from '@components/common/Header/Header'
 import styles from '@assets/Home.module.css'
 
 const Home: NextPage = () => {
